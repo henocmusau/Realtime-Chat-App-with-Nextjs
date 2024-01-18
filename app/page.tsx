c953ee5307e7de4f66d419ca0e61b96a99ff4f6e
Henoc Musau <henocmusau@gmail.com>
2024-01-18 22:42:02 +0100

    Mise en place du videoPlayer

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import VideoPlayer from '@/components/VideoPlayer'
 
 export default function Home() {
   return (
-    <main className=''>
-      <div className='text-2xl font-semibold'>Mon Super site</div>
+    <main className='flex flex-col items-center justify-center'>
+      <div className='text-2xl font-semibold flex flex-col items-center justify-center'>
+        <h1 className='text-3xl font-semibold my-3'>Webcam Video Player</h1>
+        <VideoPlayer />
+      </div>
     </main>
   )
 }
