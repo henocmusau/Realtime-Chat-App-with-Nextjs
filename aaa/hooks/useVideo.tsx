a622bee36ada33663002ecee0c3f60e415f8c9ae
Henoc Musau <henocmusau@gmail.com>
2024-01-23 13:12:25 +0100

    Creation du Videoplayer dont la source est le webcam

---
+++ aaa/hooks/useVideo.tsx
@@ -0,0 +1,26 @@
+'use client'
+import React, { LegacyRef, useEffect, useRef, useState } from 'react'
+
+export default function useVideo() {
+    const videoRef = useRef<LegacyRef<HTMLVideoElement> | undefined>()
+    const [error, setError] = useState({ state: false, msg: '' })
+    // const [stream, setStream] = useState<MediaStream | null>(null)
+
+    useEffect(() => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+            .then((currentStream) => {
+                // setStream(currentStream)
+                if (videoRef.current) videoRef.current.srcObject = currentStream
+            })
+            .catch((err: any) => {
+                console.log(err)
+                setError({ state: true, msg: 'Impossible de se connecter à la caméra' })
+            })
+    }, [])
+
+    return {
+        videoRef,
+        error,
+        // stream
+    }
+}
