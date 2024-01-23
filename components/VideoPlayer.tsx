1f2f5b8d83bc62f1d83a9ccd8bdd145900871963
Henoc Musau <henocmusau@gmail.com>
2024-01-23 23:32:32 +0100

    Ajout de la fonctionnalité de capture des photos

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,15 +1,19 @@
 'use client'
 import useVideo from '@/hooks/useVideo'
 import useWebcam from '@/hooks/useWebcam'
+import Image from 'next/image'
 import React from 'react'
 
 export default function VideoPlayer() {
-    const { videoRef, error } = useWebcam()
+    const { videoRef, error, canvasRef, photoRef, takePicture } = useWebcam()
     return (
         <section className='h-full w-full flex-col flex items-center justify-center'>
             {error.state ? <p className='p-3 my-3 text-lg font-normal rounded-lg bg-red-200'>Une erreur est survenur : {error.msg}</p> : null}
             <h2 className='font-semibold text-2xl mb-4'>My video Player</h2>
             <video playsInline autoPlay muted ref={videoRef} className='min-w-[400px] h-[60vh] aspect-video rounded-lg shadow-md' />
+            <button onClick={takePicture} className='primaryBtn'>Capturer</button>
+            <canvas id='canvas' ref={canvasRef}></canvas>
+            <Image alt='' src={''} ref={photoRef} />
         </section>
     )
 }
--- hooks/useWebcam.tsx
+++ hooks/useWebcam.tsx
@@ -1,8 +1,10 @@
 'use client'
-import React, { MutableRefObject, LegacyRef, useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 export default function useWebcam() {
     const videoRef = useRef<HTMLVideoElement>(null)
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const photoRef = useRef<HTMLImageElement>(null)
     const [error, setError] = useState({ state: false, msg: '' })
     // const [stream, setStream] = useState<MediaStream | null>(null)
 
@@ -18,9 +20,42 @@ export default function useWebcam() {
             })
     }, [])
 
+    const clearphoto = () => {
+        const context = canvasRef.current ? canvasRef.current.getContext("2d") : null
+        if (!context) return
+        context.fillStyle = "#AAA";
+        if (!canvasRef.current) return
+        context.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+        const data = canvasRef.current.toDataURL("image/png");
+
+        if (!photoRef.current) return
+        photoRef.current.setAttribute("src", data);
+    }
+
+    const width = 320; // We will scale the photo width to this
+    let height = videoRef.current ? videoRef.current.videoHeight / (videoRef.current.videoWidth / width) : 0
+
+    const takePicture = () => {
+        const context = canvasRef.current ? canvasRef.current.getContext("2d") : null
+        if (width && height && canvasRef.current && context && photoRef.current) {
+            canvasRef.current.width = width;
+            canvasRef.current.height = height;
+            context.drawImage(videoRef.current, 0, 0, width, height);
+
+            const data = canvasRef.current.toDataURL("image/png");
+            photoRef.current.setAttribute("src", data);
+        } else {
+            clearphoto();
+        }
+    }
+
     return {
         videoRef,
         error,
+        photoRef,
+        canvasRef,
+        takePicture
         // stream
     }
 }
